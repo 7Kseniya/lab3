8ff475b4f4cff616be95b1094a2328923797c785
Ksenya <114088092+7Kseniya@users.noreply.github.com>
2023-01-13 11:26:24 +0300

    Update Doors.java

--- src/ru/ifmo/se/lab3/objects/Doors.java
+++ src/ru/ifmo/se/lab3/objects/Doors.java
@@ -1,12 +1,12 @@
 package ru.ifmo.se.lab3.objects;
 
-public class Doors extends Object implements NonExisted {
-    public Doors(String object_name) {
-        super(object_name);
+public class Doors extends Objects implements NonExisted {
+    public Doors(String objects_name) {
+        super(objects_name);
     }
     @Override
     public String thereAreNo(){
-        return "ни " + getObject_name();
+        return "ни " + getObjects_name();
     }
 }
 
